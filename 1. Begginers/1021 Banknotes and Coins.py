163fc4fba37faa091ed8134f5f410efd232b6b0e
Lisandro Dalla Costa <lisandrodc06@gmail.com>
2018-06-21 01:17:12 -0300

    GG

--- 1. Begginers/1021 Banknotes and Coins.py
+++ 1. Begginers/1021 Banknotes and Coins.py
@@ -17,10 +17,7 @@ for i in range(6,12):
     n -= c[i] * p[i]
 
 # Problemas con los decimales del float
-
-
-
-
+# Wrong answer (10%)
 
 
 p = (100, 50, 20, 10, 5, 2, 1, 0.50, 0.25, 0.10, 0.05, 0.01)
@@ -28,12 +25,12 @@ p = (100, 50, 20, 10, 5, 2, 1, 0.50, 0.25, 0.10, 0.05, 0.01)
 n = float( input( ))
 print('NOTAS:')
 for i in range(0,6):
-    print('{} nota(s) de R$ {:.2f}'.format(int (n//p[i]), p[i]))
-    n = round (n - int(n//p[i]) * p[i], 2)
+    print('{} nota(s) de R$ {:.2f}'.format(int (n/p[i]), p[i]))
+    n = round (n - int(n/p[i]) * p[i], 2)
 
 print('MOEDAS:')
 for i in range(6,12):
-    print('{} moeda(s) de R$ {:.2f}'.format(int (n//p[i]), p[i]))
-    n = round (n - int(n//p[i]) * p[i], 2)
+    print('{} moeda(s) de R$ {:.2f}'.format(int (n/p[i]), p[i]))
+    n = round (n - int(n/p[i]) * p[i], 2)
 
-# Wrong answer (10%)
+# Time: 0.016s
