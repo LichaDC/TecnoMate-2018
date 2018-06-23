7fc0fbbb3309211264862c6af2eff4faf0151277
Lisandro Dalla Costa <lisandrodc06@gmail.com>
2018-06-23 00:37:21 -0300

    URI

---
+++ 1. Begginers/1001-Extremely_Basic.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int a, b;
+
+int main(){
+
+    cin >> a;
+    cin >> b;
+    cout<< "X = " << a+b << endl;
+
+
+return 0;
+}
+
+// Time 0.000s
---
+++ 1. Begginers/1002-Area_of_a_Circle.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+float pi = 3.14159;
+int r;
+
+int main(){
+    cin >> r;
+    cout<< "A=" << pi*pow(r,2) << endl;
+return 0;
+}
+
+// Time 0.000s
